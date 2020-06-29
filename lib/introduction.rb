7bebfa9ac3159ae6315fdf18ebb76286dae5836b
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-29 15:46:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,13 +6,15 @@
 
 #introduction "Njeri"
 
-def introduction_with_language (name, language)
+#def introduction_with_language (name, language)
+  #puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end 
+
+#introduction_with_language "Njeri","Ruby"
+
+def introduction_with_language_optional (name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
-
-introduction_with_language "Njeri","Ruby"
-
-
 
 
 
